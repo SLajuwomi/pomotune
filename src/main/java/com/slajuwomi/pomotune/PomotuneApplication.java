62be535f20f95b96b51d342e62d40c32b6699bda
SLajuwomi <stephenlajuwomi@gmail.com>
2025-06-12 23:41:52 -0500

    initialize java project

---
+++ src/main/java/com/slajuwomi/pomotune/PomotuneApplication.java
@@ -0,0 +1,13 @@
+package com.slajuwomi.pomotune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomotuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomotuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slajuwomi/pomotune/PomotuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slajuwomi.pomotune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomotuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
